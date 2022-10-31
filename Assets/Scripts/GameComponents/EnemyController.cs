003c62a5fdc6c526d3ccda206d12ad04399148bd
Ismail Bennani <avace.contact@gmail.com>
2022-11-01 00:57:54 +0100

    refactor

--- Assets/Scripts/GameComponents/EnemyController.cs
+++ Assets/Scripts/GameComponents/EnemyController.cs
@@ -13,12 +13,14 @@ namespace GameComponents
     {
         public long id;
 
-        void Start()
-        {
-            StartCoroutine(MoveAlongPath());
-        }
+        private EnemyState _state;
+        private WorldCell[] _path;
 
-        private IEnumerator MoveAlongPath()
+        private Vector2 _startPos;
+        private Vector2 _middlePos;
+        private Vector2 _targetPos;
+
+        void Start()
         {
             if (id <= 0)
             {
@@ -27,39 +29,43 @@ namespace GameComponents
 
             RequireGameManager();
 
-            EnemyState state = GameManager.gameState.enemyStates.SingleOrDefault(t => t.id == id);
-            if (state == null)
+            _state = GameManager.gameState.enemyStates.SingleOrDefault(t => t.id == id);
+            if (_state == null)
             {
                 throw new InvalidOperationException($"could not find enemy state with id {id}");
             }
+            
+            _path = GameManager.mapManager.GetPath();
+            
+            UpdateTargetPositions();
+        }
 
-            WorldCell[] path = GameManager.mapManager.GetPath();
-
-            for (int i = 0; i < path.Length - 1; i++)
+        void FixedUpdate()
+        {
+            if (_state.pathCellCompletion > 1)
             {
-                WorldCell previousCell = i == 0 ? path[i] : path[i - 1];
-                WorldCell currentCell = path[i];
-                WorldCell nextCell = path[i + 1];
-
-                state.pathIndex = i;
-
-                Vector2 startPos = currentCell.worldPosition - (currentCell.worldPosition - previousCell.worldPosition) / 2;
-                Vector2 middlePos = currentCell.worldPosition;
-                Vector2 targetPos = currentCell.worldPosition + (nextCell.worldPosition - currentCell.worldPosition) / 2;
-
-                float progression = 0;
-                while (progression <= 1)
-                {
-                    transform.position = progression < 0.5
-                        ? Vector2.Lerp(startPos, middlePos, progression * 2).WithDepth(transform.position.z)
-                        : Vector2.Lerp(middlePos, targetPos, progression * 2 - 1).WithDepth(transform.position.z);
-
-                    progression += Time.fixedDeltaTime * state.config.speed;
-                    state.pathCellCompletion = progression;
-
-                    yield return new WaitForFixedUpdate();
-                }
+                _state.pathIndex++;
+                _state.pathCellCompletion -= 1;
+                
+                UpdateTargetPositions();
             }
+            
+            transform.position = _state.pathCellCompletion < 0.5
+                ? Vector2.LerpUnclamped(_startPos, _middlePos, _state.pathCellCompletion * 2).WithDepth(transform.position.z)
+                : Vector2.LerpUnclamped(_middlePos, _targetPos, _state.pathCellCompletion * 2 - 1).WithDepth(transform.position.z);
+
+            _state.pathCellCompletion += Time.fixedDeltaTime * _state.config.speed;
+        }
+
+        private void UpdateTargetPositions()
+        {
+            WorldCell previousCell = _state.pathIndex == 0 ? _path[_state.pathIndex] : _path[_state.pathIndex - 1];
+            WorldCell currentCell = _path[_state.pathIndex];
+            WorldCell nextCell = _path[_state.pathIndex + 1];
+
+            _startPos = currentCell.worldPosition - (currentCell.worldPosition - previousCell.worldPosition) / 2;
+            _middlePos = currentCell.worldPosition;
+            _targetPos = currentCell.worldPosition + (nextCell.worldPosition - currentCell.worldPosition) / 2;
         }
     }
 }
